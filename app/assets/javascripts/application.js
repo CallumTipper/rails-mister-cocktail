4b4f4d7df64e272ecc437a0291b7aa4153e04fb9
Callum Tipper <callum.tipper@gmail.com>
2018-08-02 19:33:20 +0800

    just struggling with scss now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var button = document.getElementById("add-button");
+
+// Get the offset position of the navbar
+var sticky = button.offsetTop;
+
+// Add the sticky class to the button when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    button.classList.add("sticky");
+  } else {
+    button.classList.remove("sticky");
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,21 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+import 'bootstrap';
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var button = document.getElementById("add-button");
+
+// Get the offset position of the navbar
+var sticky = button.offsetTop;
+
+// Add the sticky class to the button when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    button.classList.add("sticky");
+  } else {
+    button.classList.remove("sticky");
+  }
+}
